748925b6a68c65a7371b725105aae35d7f2ebad1
Nonlone <thunderbird.shun@gmail.com>
2017-04-29 17:38:51 +0800

    测试Model类和Dao类

---
+++ src/main/java/per/nonlone/spanner/simple/dao/TestModelDao.java
@@ -0,0 +1,21 @@
+package per.nonlone.spanner.simple.dao;
+
+import org.nutz.dao.Dao;
+import org.nutz.ioc.loader.annotation.Inject;
+import org.nutz.ioc.loader.annotation.IocBean;
+
+import per.nonlone.spanner.simple.model.TestModel;
+
+@IocBean
+public class TestModelDao {
+	
+	private Class<?> entityClass = TestModel.class;
+	
+	@Inject
+	private Dao dao;
+	
+	public TestModel insert(TestModel testModel){
+		return dao.insert(testModel);
+	}
+
+}
---
+++ src/main/java/per/nonlone/spanner/simple/model/TestModel.java
@@ -0,0 +1,33 @@
+package per.nonlone.spanner.simple.model;
+
+import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.Id;
+import org.nutz.dao.entity.annotation.Table;
+
+@Table("TB_TEST")
+public class TestModel {
+
+	@Id
+	private Integer id;
+	
+	@Column("VALUE")
+	private String value;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
+}
