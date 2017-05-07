316b273a700b921e9aad7a117702494138ab15ff
Nonlone <thunderbird.shun@gmail.com>
2017-05-07 23:58:47 +0800

    增加Spring事务方法

--- src/main/java/per/nonlone/spanner/simple/service/SimpleSpringService.java
+++ src/main/java/per/nonlone/spanner/simple/service/SimpleSpringService.java
@@ -1,8 +1,16 @@
 package per.nonlone.spanner.simple.service;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import per.nonlone.spanner.simple.dao.TestModelDao;
+import per.nonlone.spanner.simple.model.TestModel;
 
 @Service
 public class SimpleSpringService {
@@ -10,7 +18,21 @@ public class SimpleSpringService {
 	@Resource(name="simpleSpringSubService")
 	private SimpleSpringSubService simpleSpringSubService;
 	
+	@Autowired
+	@Lazy
+	private TestModelDao testModelDao;
+	
 	public String process(){
 		return "this is spring service hashcode>"+hashCode();
 	}
+	
+	@Transactional
+    public List<TestModel>  insertTestModel(String... values){
+        return testModelDao.insertTestModel(values);
+    }
+
+    @Transactional
+    public List<TestModel> insertTestModelInterrupted(String... values){
+        return testModelDao.insertTestModelInterrupted(values);
+    }
 }
