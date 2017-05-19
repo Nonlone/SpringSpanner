d649bc7608018db5498decbfa83c586793369cd1
leishunyang <leishunyang@ppmoney.com>
2017-05-19 20:57:00 +0800

    增加注入NutzTransDataSource

--- src/main/java/per/nonlone/spanner/simple/aop/SimpleSpringTransactionInterceptor.java
+++ src/main/java/per/nonlone/spanner/simple/aop/SimpleSpringTransactionInterceptor.java
@@ -1,38 +1,31 @@
 package per.nonlone.spanner.simple.aop;
 
-import javax.annotation.PostConstruct;
-
 import org.apache.log4j.Logger;
 import org.nutz.aop.InterceptorChain;
 import org.nutz.aop.MethodInterceptor;
 import org.nutz.trans.Trans;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.jta.ManagedTransactionAdapter;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 
-import per.nonlone.spanner.nutz.NutzTransactionAdapter;
+import per.nonlone.spanner.nutz.NutzTransDataSource;
 import per.nonlone.spanner.simple.exception.AbortTransactionException;
 
 @Component
 public class SimpleSpringTransactionInterceptor implements MethodInterceptor {
 
 	private final static Logger logger = Logger.getLogger(SimpleSpringTransactionInterceptor.class);
-
+	
 	@Autowired
-	private TransactionTemplate transactionTemplate;
-
-	@PostConstruct
-	public void init() {
-//		Trans.setup(NutzTransactionAdapter.class);
-	}
+	private NutzTransDataSource dataSource;
 
 	public void filter(InterceptorChain chain) throws Throwable {
 		final InterceptorChain transChain = chain;
 		Trans.begin();
-		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+		new TransactionTemplate(new DataSourceTransactionManager(dataSource)).execute(new TransactionCallbackWithoutResult() {
 			@Override
 			protected void doInTransactionWithoutResult(TransactionStatus status) {
 				try {
@@ -40,7 +33,6 @@ public class SimpleSpringTransactionInterceptor implements MethodInterceptor {
 				} catch (Throwable throwable) {
 					logger.error(throwable);
 					status.setRollbackOnly();
-//	                    throw new AbortTransactionException(throwable);
 				} finally {
 					try {
 						Trans.close();
@@ -51,5 +43,4 @@ public class SimpleSpringTransactionInterceptor implements MethodInterceptor {
 			}
 		});
 	}
-
 }
