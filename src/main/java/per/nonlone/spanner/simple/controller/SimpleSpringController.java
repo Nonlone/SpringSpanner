adf542a30526b5b6b44502f8532d6ac4a75ef82f
Nonlone <thunderbird.shun@gmail.com>
2017-05-21 18:31:30 +0800

    增加Fastjson，Jackson包，支持Spring返回JSON形式，增加Nutz兼容Spring的SpringDaoRunner，重命名springServlet为springMVCServlet

--- src/main/java/per/nonlone/spanner/simple/controller/SimpleSpringController.java
+++ src/main/java/per/nonlone/spanner/simple/controller/SimpleSpringController.java
@@ -1,11 +1,16 @@
 package per.nonlone.spanner.simple.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
 
 import per.nonlone.spanner.simple.service.SimpleNutzService;
 import per.nonlone.spanner.simple.service.SimpleSpringService;
@@ -32,23 +37,47 @@ public class SimpleSpringController {
 		return simpleNutzSerivce.process()+":"+simpleNutzSerivce.hashCode()+" , "+simpleSpringService.process() + ":"+simpleSpringService.hashCode();
 	}
 	
+	@RequestMapping("/spring/indexArray")
+	@ResponseBody
+	public String[] indexArray(){
+		return new String[]{
+            RandomStringUtils.randomAlphanumeric(4),
+            RandomStringUtils.randomAlphanumeric(4),
+            RandomStringUtils.randomAlphanumeric(4),
+            RandomStringUtils.randomAlphanumeric(4),
+            RandomStringUtils.randomAlphanumeric(4),
+            RandomStringUtils.randomAlphanumeric(4)
+		};
+    }
+	
+	@RequestMapping("/spring/indexList")
+	@ResponseBody
+	public List<String> indexList(){
+		return new ArrayList<String>(){{
+			for(int i=0;i<4;i++){
+				this.add(RandomStringUtils.randomAlphanumeric(4));
+			}
+		}};
+    }
+	
+	
 	@RequestMapping("/spring/insert")
     @ResponseBody
-    public Object insert(){
-        return simpleSpringService.insertTestModel(new String[]{
+    public String insert(){
+        return JSON.toJSONString(simpleSpringService.insertTestModel(new String[]{
                 RandomStringUtils.randomAlphanumeric(4),
                 RandomStringUtils.randomAlphanumeric(4),
                 RandomStringUtils.randomAlphanumeric(4),
                 RandomStringUtils.randomAlphanumeric(4),
                 RandomStringUtils.randomAlphanumeric(4),
                 RandomStringUtils.randomAlphanumeric(4)
-        });
+        }));
     }
 	
 	@RequestMapping("/spring/insert2")
     @ResponseBody
     public Object insert2(){
-        return simpleSpringService.insertTestModel(new String[]{
+        return simpleSpringService.insertTestModelInterrupted(new String[]{
                 RandomStringUtils.randomAlphanumeric(4),
                 RandomStringUtils.randomAlphanumeric(4),
                 RandomStringUtils.randomAlphanumeric(4),
@@ -63,7 +92,7 @@ public class SimpleSpringController {
     public Object insertWithPropagation(){
         return simpleSpringService.insertTestModelWithPropagation(new String[]{
                 RandomStringUtils.randomAlphanumeric(4),
-                RandomStringUtils.randomAlphanumeric(4),
+                RandomStringUtils.randomAlphanumeric(4),	
                 RandomStringUtils.randomAlphanumeric(4),
                 RandomStringUtils.randomAlphanumeric(4),
                 RandomStringUtils.randomAlphanumeric(4),
