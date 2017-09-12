29e47a554a3668445bcd2eb273761730030d745a
Nonlone <thunderbird.shun@gmail.com>
2017-09-13 00:30:04 +0800

    修正配置文件，不需要提供数据库连接

--- src/main/java/per/nonlone/spanner/simple/aop/SimpleSpringTransactionInterceptor.java
+++ src/main/java/per/nonlone/spanner/simple/aop/SimpleSpringTransactionInterceptor.java
@@ -19,7 +19,7 @@ public class SimpleSpringTransactionInterceptor implements MethodInterceptor {
 
 	private final static Logger logger = Logger.getLogger(SimpleSpringTransactionInterceptor.class);
 	
-	@Autowired
+//	@Autowired
 	private NutzTransDataSource dataSource;
 
 	public void filter(InterceptorChain chain) throws Throwable {
