0967ac84730f64f90578c77a31fc9ed7fff8b58d
Nonlone <thunderbird.shun@gmail.com>
2017-09-25 07:36:54 +0800

    修改使用Spring的事务异常类

--- src/main/java/per/nonlone/spanner/simple/aop/SimpleSpringTransactionInterceptor.java
+++ src/main/java/per/nonlone/spanner/simple/aop/SimpleSpringTransactionInterceptor.java
@@ -7,46 +7,51 @@ import org.nutz.trans.Trans;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.TransactionException;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import per.nonlone.spanner.nutz.NutzTransDataSource;
 import per.nonlone.spanner.simple.exception.AbortTransactionException;
 
+import javax.transaction.TransactionRolledbackException;
+
 @Component
 public class SimpleSpringTransactionInterceptor implements MethodInterceptor {
 
-	private final static Logger logger = Logger.getLogger(SimpleSpringTransactionInterceptor.class);
-	
-	@Autowired
-	private NutzTransDataSource dataSource;
+    private final static Logger logger = Logger.getLogger(SimpleSpringTransactionInterceptor.class);
 
-	public void filter(InterceptorChain chain) throws Throwable {
-		final InterceptorChain transChain = chain;
-		Trans.begin();
-		new TransactionTemplate(new DataSourceTransactionManager(dataSource)).execute(new TransactionCallbackWithoutResult() {
-			@Override
-			protected void doInTransactionWithoutResult(TransactionStatus status) {
-				try {
-					transChain.doChain();
-				} catch (Throwable throwable) {
-					logger.error(throwable);
-					status.setRollbackOnly();
-					try {
-						Trans.rollback();
-					} catch (Exception e) {
-						//Nutz 回滚失败
-						throw new AbortTransactionException(e);
-					}
-				} finally {
-					try {
-						Trans.close();
-					} catch (Exception e) {
-						throw new AbortTransactionException(e);
-					}
-				}
-			}
-		});
-	}
+    @Autowired
+    private NutzTransDataSource dataSource;
+
+    public void filter(InterceptorChain chain) throws Throwable {
+        final InterceptorChain transChain = chain;
+        Trans.begin();
+        new TransactionTemplate(new DataSourceTransactionManager(dataSource)).execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                try {
+                    transChain.doChain();
+                } catch (Throwable throwable) {
+                    logger.error(throwable);
+                    status.setRollbackOnly();
+                    try {
+                        Trans.rollback();
+                    } catch (Exception e) {
+                        //Nutz 回滚失败
+                        throw new TransactionSystemException(String.format("Nutz Trans.rollback error %s", e.getMessage()), e);
+                    }
+                } finally {
+                    try {
+                        Trans.close();
+                    } catch (Exception e) {
+                        // Nutz 关闭失败
+                        throw new TransactionSystemException(String.format("Nutz Trans.close error %s", e.getMessage()), e);
+                    }
+                }
+            }
+        });
+    }
 }
