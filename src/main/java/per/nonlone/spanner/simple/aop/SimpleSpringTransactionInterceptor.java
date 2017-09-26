89646c277cdf0d9d801ae909fd2804e519131d50
Nonlone <thunderbird.shun@gmail.com>
2017-09-27 01:06:53 +0800

    检测是否在声明式事务中，使用不同事务管理器进行事务

--- src/main/java/per/nonlone/spanner/simple/aop/SimpleSpringTransactionInterceptor.java
+++ src/main/java/per/nonlone/spanner/simple/aop/SimpleSpringTransactionInterceptor.java
@@ -7,15 +7,18 @@ import org.nutz.trans.Trans;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.TransactionException;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.TransactionSystemException;
+import org.springframework.transaction.*;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import per.nonlone.spanner.nutz.NutzTransDataSource;
 import per.nonlone.spanner.simple.exception.AbortTransactionException;
 
+import javax.sql.DataSource;
+import javax.transaction.TransactionManager;
 import javax.transaction.TransactionRolledbackException;
 
 @Component
@@ -24,12 +27,22 @@ public class SimpleSpringTransactionInterceptor implements MethodInterceptor {
     private final static Logger logger = Logger.getLogger(SimpleSpringTransactionInterceptor.class);
 
     @Autowired
+    private PlatformTransactionManager platformTransactionManager;
+
+    @Autowired
     private NutzTransDataSource dataSource;
 
     public void filter(InterceptorChain chain) throws Throwable {
         final InterceptorChain transChain = chain;
+        //默认为Spring 事务管理
+        PlatformTransactionManager txManager = this.platformTransactionManager;
+        try {
+            TransactionAspectSupport.currentTransactionStatus();
+        } catch (NoTransactionException nte) {
+            txManager = new DataSourceTransactionManager(dataSource);
+        }
         Trans.begin();
-        new TransactionTemplate(new DataSourceTransactionManager(dataSource)).execute(new TransactionCallbackWithoutResult() {
+        new TransactionTemplate(txManager).execute(new TransactionCallbackWithoutResult() {
             @Override
             protected void doInTransactionWithoutResult(TransactionStatus status) {
                 try {
@@ -37,21 +50,14 @@ public class SimpleSpringTransactionInterceptor implements MethodInterceptor {
                 } catch (Throwable throwable) {
                     logger.error(throwable);
                     status.setRollbackOnly();
-                    try {
-                        Trans.rollback();
-                    } catch (Exception e) {
-                        //Nutz 回滚失败
-                        throw new TransactionSystemException(String.format("Nutz Trans.rollback error %s", e.getMessage()), e);
-                    }
-                } finally {
-                    try {
-                        Trans.close();
-                    } catch (Exception e) {
-                        // Nutz 关闭失败
-                        throw new TransactionSystemException(String.format("Nutz Trans.close error %s", e.getMessage()), e);
-                    }
                 }
             }
         });
+        try {
+            Trans.close();
+        } catch (Exception e) {
+            // Nutz 关闭失败
+            throw new TransactionSystemException(String.format("Nutz Trans.close error %s", e.getMessage()), e);
+        }
     }
 }
